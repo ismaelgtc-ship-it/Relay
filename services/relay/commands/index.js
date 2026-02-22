5231df9769b6a0c7724b91d569a63bd7a42c710b
GOGETA <ismael.gtc@gmail.com>
2026-02-22 09:40:16 +0100

    Add files via upload

--- services/relay/commands/index.js
+++ services/relay/commands/index.js
@@ -1,1 +1,11 @@
+import ping from "./ping.js";
 
+export const commandRegistry = new Map();
+
+export function loadCommands() {
+  const commands = [ping];
+  for (const cmd of commands) {
+    commandRegistry.set(cmd.data.name, cmd);
+  }
+  return commands.map(c => c.data);
+}
